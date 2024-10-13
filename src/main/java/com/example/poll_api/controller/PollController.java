9696ed8aeb4b61b27ab93120b413f036602cdc77
Abdul Basit <fayyaz855@gmail.com>
2024-10-13 22:58:32 +0500

    Cross Origin Updated
    Cross Origin for Front-End Integration

--- src/main/java/com/example/poll_api/controller/PollController.java
+++ src/main/java/com/example/poll_api/controller/PollController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.poll_api.model.Poll;
 import com.example.poll_api.service.PollService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/poll")
 public class PollController {
